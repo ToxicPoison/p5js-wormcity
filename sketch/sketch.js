8c7fc70f83e7b7ba01c023c0c0abb35dbf82cf1c
Kyle <46309993+ToxicPoison@users.noreply.github.com>
2023-09-20 05:07:57 +0000

    Background buildings

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,13 +1,69 @@
+var numBuildings = 100;
+var windowPercent = 0.1;
+var maxSize = 0.3;
+
 function setup() {
   createCanvas(400, 400);
+  background("#001437");
+
+  baseCol = color(0, 0, 0);
+  var windowColor = color(150, 2, 100);
+  
+  noStroke();
+
+  // BUILDINGS
+  for (var i = 0; i < numBuildings; i++)
+  {
+    fill(baseCol);
+    
+
+    
+    var scaleX = width * Math.random() * maxSize;
+    var scaleY = height * Math.random() * maxSize;
+    var posX = width * Math.random() - .5*scaleX;
+    var posY = height * Math.random() - .5*scaleY;
+
+    rect(
+      posX,
+      posY,
+      scaleX,
+      scaleY
+    );
+
+    fill(color(((i * 2)/ numBuildings) * 5, ((i * 2)/ numBuildings) * 30, ((i * 2)/ numBuildings) * 50));
+
+    rect(
+      posX,
+      posY,
+      scaleX * 0.1,
+      scaleY * 1
+    );
+    
+    // WINDOWS
+    var winWid = max(Math.random() * 5, 1);
+    var winHi = max(Math.random() * 5, 1);
+
+
+    for (var x1 = 0; x1 < (scaleX - winWid); x1 += 10)
+    {
+      for (var y1 = 0; y1 < (scaleY - winHi); y1 += 10)
+      {
+        
+        fill(windowColor);
+        if (Math.random() > windowPercent)
+        {
+          fill("black");
+        }
+        rect(x1 + posX, y1 + posY, winWid, winHi);
+      }
+    }
+
+    baseCol = color((i / numBuildings) * 5, (i / numBuildings) * 30, (i / numBuildings) * 50);
+
+    
+  }
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(5, 163, 255);
-  }
-
-  ellipse(mouseX, mouseY, 80, 80);
+  //ahem. maah! maah! maah!
 }
